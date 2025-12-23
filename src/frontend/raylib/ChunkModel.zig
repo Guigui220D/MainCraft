dd47697c354b9bea4fabafecfeb06e0857b83d01
Guillaume Derex <gderex8@orange.fr>
2025-12-23 22:20:19 +0100

    Restarted chunk mesh building to not share indices

--- src/frontend/raylib/ChunkModel.zig
+++ src/frontend/raylib/ChunkModel.zig
@@ -56,76 +56,91 @@ pub fn deinit(self: ChunkModel, alloc: std.mem.Allocator) void {
 
 /// Renders a chunk into a mesh
 fn generateMeshForChunk(chunk: Chunk) !rl.Mesh {
-    // Generate vertex matrix
-    // TODO: do that comptime or only once
-    var vertices: std.ArrayList(f32) = try .initCapacity(rl.mem, (17 * 17 * 129 * 3));
+    var vertices: std.ArrayList(f32) = try .initCapacity(rl.mem, (std.math.maxInt(c_ushort) * 3 + 1));
     errdefer vertices.deinit(rl.mem);
 
-    {
-        var x: usize = 0;
-        while (x <= 16) : (x += 1) {
-            var z: usize = 0;
-            while (z <= 16) : (z += 1) {
-                var y: usize = 0;
-                while (y <= 128) : (y += 1) {
-                    vertices.appendSliceAssumeCapacity(&.{
-                        @floatFromInt(x),
-                        @floatFromInt(y),
-                        @floatFromInt(z),
-                    });
-                }
-            }
-        }
-    }
-
-    // Add cubes (TODO: cull hidden faces)
     var indices: std.ArrayList(c_ushort) = .{};
     errdefer indices.deinit(rl.mem);
 
-    var normals: std.ArrayList(f32) = .{};
-    errdefer normals.deinit(rl.mem);
-
-    var colors: std.ArrayList(u8) = .{};
+    var colors: std.ArrayList(u32) = .{};
     errdefer colors.deinit(rl.mem);
 
-    for (chunk.blocks, 0..) |id, i| {
-        if (id == 0)
+    var id: c_ushort = 0;
+    for (chunk.blocks, 0..) |block_id, i| {
+        if (block_id == 0)
             continue;
 
         const y: i32 = @intCast(i % 128);
         const z: i32 = @intCast(i / 128 % 16);
         const x: i32 = @intCast(i / (128 * 16));
-        const block = coord.Block{ .x = x, .y = y, .z = z };
 
-        // For each face
-        const top_face = getFaceVertices(block, .positive, .y);
-        const bottom_face = getFaceVertices(block, .negative, .y);
-        const east_face = getFaceVertices(block, .positive, .x);
-        const west_face = getFaceVertices(block, .negative, .x);
-        const south_face = getFaceVertices(block, .positive, .z);
-        const north_face = getFaceVertices(block, .negative, .z);
+        // TODO: support for multiple meshes per chunk
+        if (id > std.math.maxInt(c_ushort) - 8)
+            break; // Can't fit more vertices
 
-        inline for (&.{ top_face, bottom_face, east_face, west_face, south_face, north_face }) |face| {
-            // Add the two triangles
-            try indices.appendSlice(rl.mem, &face);
+        // TODO: helper function for that
+        // TODO: model per block id (reuse vertices where possible)
+        vertices.appendSliceAssumeCapacity(&.{
+            // 0
+            @floatFromInt(x),
+            @floatFromInt(y),
+            @floatFromInt(z),
+            // 1
+            @floatFromInt(x),
+            @floatFromInt(y),
+            @floatFromInt(z + 1),
+            // 2
+            @floatFromInt(x + 1),
+            @floatFromInt(y),
+            @floatFromInt(z),
+            // 3
+            @floatFromInt(x + 1),
+            @floatFromInt(y),
+            @floatFromInt(z + 1),
+            // 4
+            @floatFromInt(x),
+            @floatFromInt(y + 1),
+            @floatFromInt(z),
+            // 5
+            @floatFromInt(x),
+            @floatFromInt(y + 1),
+            @floatFromInt(z + 1),
+            // 6
+            @floatFromInt(x + 1),
+            @floatFromInt(y + 1),
+            @floatFromInt(z),
+            // 7
+            @floatFromInt(x + 1),
+            @floatFromInt(y + 1),
+            @floatFromInt(z + 1),
+        });
 
-            // TODO: correct normals
-            try normals.appendSlice(rl.mem, &.{
-                // Triangle 1
-                0,
-                1,
-                0,
-                // Triangle 2
-                0,
-                1,
-                0,
-            });
+        // TODO: helper function for that
+        try indices.appendSlice(rl.mem, &.{
+            id + 0, id + 2, id + 3,
+            id + 0, id + 3, id + 1,
+            id + 1, id + 3, id + 7,
+            id + 1, id + 7, id + 5,
+            id + 0, id + 6, id + 2,
+            id + 0, id + 4, id + 6,
+            id + 0, id + 1, id + 5,
+            id + 0, id + 5, id + 4,
+            id + 4, id + 7, id + 6,
+            id + 4, id + 5, id + 7,
+            id + 2, id + 7, id + 3,
+            id + 2, id + 6, id + 7,
+        });
 
-            // TODO: what's going on with colors?
-            try colors.appendSlice(rl.mem, &.{ 255, 0, 0, 255 });
-            try colors.appendSlice(rl.mem, &.{ 0, 255, 0, 255 });
-            try colors.appendSlice(rl.mem, &.{ 0, 0, 255, 255 });
-        }
+        try colors.append(rl.mem, 0xff000000);
+        try colors.append(rl.mem, 0xff000099);
+        try colors.append(rl.mem, 0xff009900);
+        try colors.append(rl.mem, 0xff009999);
+        try colors.append(rl.mem, 0xff990000);
+        try colors.append(rl.mem, 0xff990099);
+        try colors.append(rl.mem, 0xff999900);
+        try colors.append(rl.mem, 0xff999999);
+
+        id += 8;
     }
 
     const tri_count = indices.items.len / 3;
@@ -142,7 +157,7 @@ fn generateMeshForChunk(chunk: Chunk) !rl.Mesh {
         .boneWeights = @ptrFromInt(0),
         .colors = @ptrCast(try colors.toOwnedSlice(rl.mem)),
         .indices = @ptrCast(try indices.toOwnedSlice(rl.mem)),
-        .normals = @ptrCast(try normals.toOwnedSlice(rl.mem)),
+        .normals = @ptrFromInt(0),
         .tangents = @ptrFromInt(0),
         .texcoords = @ptrFromInt(0),
         .texcoords2 = @ptrFromInt(0),
@@ -152,49 +167,4 @@ fn generateMeshForChunk(chunk: Chunk) !rl.Mesh {
         .vertexCount = @intCast(vert_count),
         .vertices = @ptrCast(try vertices.toOwnedSlice(rl.mem)),
     };
-}
-
-fn getFaceVertices(block: coord.Block, comptime direction: enum { negative, positive }, comptime axis: enum { x, y, z }) [6]c_ushort {
-    const dir_bool = (direction == .positive);
-    const v = switch (axis) {
-        .x => [4]c_ushort{
-            getVertexIndexFromBlockCoords(block, false, false, dir_bool),
-            getVertexIndexFromBlockCoords(block, false, true, dir_bool),
-            getVertexIndexFromBlockCoords(block, true, false, dir_bool),
-            getVertexIndexFromBlockCoords(block, true, true, dir_bool),
-        },
-        .y => [4]c_ushort{
-            getVertexIndexFromBlockCoords(block, dir_bool, false, false),
-            getVertexIndexFromBlockCoords(block, dir_bool, false, true),
-            getVertexIndexFromBlockCoords(block, dir_bool, true, false),
-            getVertexIndexFromBlockCoords(block, dir_bool, true, true),
-        },
-        .z => [4]c_ushort{
-            getVertexIndexFromBlockCoords(block, false, dir_bool, false),
-            getVertexIndexFromBlockCoords(block, true, dir_bool, false),
-            getVertexIndexFromBlockCoords(block, false, dir_bool, true),
-            getVertexIndexFromBlockCoords(block, true, dir_bool, true),
-        },
-    };
-
-    if (dir_bool) {
-        return [6]c_ushort{ v[0], v[3], v[1], v[0], v[2], v[3] };
-    } else {
-        return [6]c_ushort{ v[0], v[1], v[3], v[0], v[3], v[2] };
-    }
-}
-
-/// Calculate index of a vertex using the block coordinates and selecting the corner with the booleans
-/// south: increasing in the z direction
-/// east: increasing in the x direction
-inline fn getVertexIndexFromBlockCoords(block: coord.Block, up: bool, south: bool, east: bool) c_ushort {
-    std.debug.assert(block.isWithinChunk());
-
-    var ret: c_ushort = 0;
-
-    ret += @intCast((block.x + @intFromBool(east)) * 129 * 17);
-    ret += @intCast((block.z + @intFromBool(south)) * 129);
-    ret += @intCast((block.y + @intFromBool(up)));
-
-    return ret;
 }
